2402c963a4ec0606f99c8ede7534fc5f771a10a6
qaraluch <prezenciq@gmail.com>
2017-10-18 17:52:55 +0200

    Import module from atv-import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
-function XXX() {
-  return {};
+import { curry } from "qm-fp";
+
+export function mdHeaderCreate(type = "all", flags) {
+  // all or 1 - 6
+  let lvl;
+  if (typeof type === "number" && /[1-6]/g.test(type)) {
+    lvl = type;
+  } else {
+    lvl = "1,6";
+  }
+  const rgx = `^(#{${lvl}})\\ ([\\w,-]+)([\\ ]?#{${lvl}})?`;
+  return new RegExp(rgx, flags);
 }
 
-export default XXX;
+export function ifMdHeader(rgx = mdHeaderCreate("all"), txt) {
+  const rgxTest = (rgx, txt) => rgx.test(txt);
+  return curry(rgxTest, rgx, txt);
+}
+
+// result example:
+// [ '#### terefere',
+//   '####',
+//   'terefere', // index [2]
+//   undefined,
+//   index: 0,
+//   input: '#### terefere \n' ]
+//   or null
+export function mdHeaderGetTxt(rgx = mdHeaderCreate("all"), txt) {
+  const rgxPull = (rgx, txt) => {
+    const result = rgx.exec(txt);
+    return result && result[2];
+  };
+  return curry(rgxPull, rgx, txt);
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,135 @@
 import test from "ava";
-import XXX from "../dist/index.js";
+import { mdHeaderCreate, mdHeaderGetTxt, ifMdHeader } from "../dist/index.js";
 
-test("constructor function check", t => {
-  const msg = "should be a function i.e. construction fn.";
-  const actual = typeof XXX === "function";
+test("mdHeaderCreate() - is function", t => {
+  const msg = "should be a function ";
+  const actual = typeof mdHeaderCreate === "function";
   const expected = true;
   t.is(actual, expected, msg);
 });
 
-test("init", t => {
-  const msg1 = "should construction fn. return an object i.e. state";
-  const actual1 = typeof XXX() === "object";
-  const expected1 = true;
-  t.is(actual1, expected1, msg1);
+test("mdHeaderCreate() - is regex object", t => {
+  const msg = "should be regex object";
+  const actual = mdHeaderCreate() instanceof RegExp;
+  const expected = true;
+  t.is(actual, expected, msg);
+});
+
+var cases = [
+  ["# terefere \n", undefined, true],
+  ["# terefere#\n", undefined, true],
+  ["# terefere # \n", undefined, true],
+
+  ["# terefere \n", 1, true],
+  ["## terefere \n", 2, true],
+  ["### terefere \n", 3, true],
+  ["#### terefere \n", 4, true],
+  ["##### terefere \n", 5, true],
+  ["###### terefere \n", 6, true],
+
+  ["## terefere \n", 1, false],
+  ["### terefere \n", 2, false],
+  ["#### terefere \n", 3, false],
+  ["##### terefere \n", 4, false],
+  ["###### terefere \n", 5, false],
+  ["####### terefere \n", 6, false],
+
+  ["# terefere \n", "all", true],
+  ["## terefere \n", "all", true],
+  ["### terefere \n", "all", true],
+  ["#### terefere \n", "all", true],
+  ["##### terefere \n", "all", true],
+  ["###### terefere \n", "all", true],
+
+  ["# terefere \n", "wrongParam", true],
+  ["## terefere \n", "wrongParam", true],
+  ["### terefere \n", "wrongParam", true],
+  ["#### terefere \n", "wrongParam", true],
+  ["##### terefere \n", "wrongParam", true],
+  ["###### terefere \n", "wrongParam", true],
+
+  ["####### terefere \n", 7, false],
+  ["####### terefere \n", "terefere", false],
+  ["####### terefere \n", "all", false],
+
+  ["#terefere# \n", undefined, false],
+  [" # terefere \n", undefined, false],
+  ["   #terefere#\n", undefined, false]
+];
+
+test("ifMdHeader() - is it md header", t => {
+  const msg = "should be heder";
+  cases.forEach((c, i) => {
+    const txt = c[0];
+    const whatHeader = c[1];
+    const predefinedResult = c[2];
+    const rgx = mdHeaderCreate(whatHeader);
+    const actual = ifMdHeader(rgx, txt);
+    const expected = predefinedResult;
+    t.is(actual, expected, `${msg} in case no. ${i} -> ${c}`);
+  });
+});
+
+var casesTxt = [
+  ["# terefere \n", undefined, "terefere"],
+  ["# terefere#\n", undefined, "terefere"],
+  ["# terefere # \n", undefined, "terefere"],
+
+  ["# terefere \n", 1, "terefere"],
+  ["## terefere \n", 2, "terefere"],
+  ["### terefere \n", 3, "terefere"],
+  ["#### terefere \n", 4, "terefere"],
+  ["##### terefere \n", 5, "terefere"],
+  ["###### terefere \n", 6, "terefere"],
+
+  ["# terefere \n", "all", "terefere"],
+  ["## terefere \n", "all", "terefere"],
+  ["### terefere \n", "all", "terefere"],
+  ["#### terefere \n", "all", "terefere"],
+  ["##### terefere \n", "all", "terefere"],
+  ["###### terefere \n", "all", "terefere"],
+
+  ["# terefere \n", "wrongParam", "terefere"],
+  ["## terefere \n", "wrongParam", "terefere"],
+  ["### terefere \n", "wrongParam", "terefere"],
+  ["#### terefere \n", "wrongParam", "terefere"],
+  ["##### terefere \n", "wrongParam", "terefere"],
+  ["###### terefere \n", "wrongParam", "terefere"],
+
+  ["####### terefere \n", 7, null],
+  ["####### terefere \n", "terefere", null],
+  ["####### terefere \n", "all", null],
+
+  ["#terefere# \n", undefined, null],
+  [" # terefere \n", undefined, null],
+  ["   #terefere#\n", undefined, null]
+];
+
+test("mdHeaderGetTxt() - return header text", t => {
+  const msg = "should be heder";
+  casesTxt.forEach((c, i) => {
+    const txt = c[0];
+    const whatHeader = c[1];
+    const predefinedResult = c[2];
+    const rgx = mdHeaderCreate(whatHeader);
+    const actual = mdHeaderGetTxt(rgx, txt);
+    const expected = predefinedResult;
+    t.is(actual, expected, `${msg} in case no. ${i} -> ${c}`);
+  });
+});
+
+test("curried ifMdHeader()", t => {
+  const msg = "curried fn should work as usual one";
+  const rgx = mdHeaderCreate(4);
+  const actual = ifMdHeader(rgx)("#### terefere \n");
+  const expected = true;
+  t.is(actual, expected, msg);
+});
+
+test("curried mdHeaderGetTxt()", t => {
+  const msg = "curried fn should work as usual one";
+  const rgx = mdHeaderCreate(4);
+  const actual = mdHeaderGetTxt(rgx)("#### terefere \n");
+  const expected = "terefere";
+  t.is(actual, expected, msg);
 });
